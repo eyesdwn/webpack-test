7b75fd5ca288d5fe59710671d8acca398196b80d
Tatiana <eyesdwn@gmail.com>
2019-11-10 17:29:36 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import users from "./users";
+import devices from "./devices";
+import projects from "./projects";
+
+const getUsersByProjectId = id => users.filter(elem => elem.projectId === id);
+const getDevicesByProjectId = id =>
+  devices.filter(elem => elem.projectId === id);
+
+const generateUsersLayout = users =>
+  users
+    .map(
+      user => `
+<p data-app-user-id=${user.appuserId} data-project-id=${user.projectId}>${user.firstName} ${user.lastName}</p>`
+    )
+    .join("");
+
+const generateDevicesLayout = devices =>
+  devices
+    .map(
+      device => `
+ <p data-device-id="${device.deviceId}" data-project-id="${device.projectId}">${device.serialNumber}</p>
+`
+    )
+    .join("");
+
+const generateProjectLayout = project => {
+  const users = getUsersByProjectId(project.id);
+  const devices = getDevicesByProjectId(project.id);
+
+  return `
+    <div class="wrapper">
+        <p>${project.title}</p>
+        ${generateUsersLayout(users)}
+        ${generateDevicesLayout(devices)}
+        ${project.beginDate ? "<p>" + project.beginDate + "</p>" : ""}
+        ${project.expirationDate ? "<p>" + project.expirationDate + "</p>" : ""}
+    </div>
+    `;
+};
+
+const layout = projects.map(generateProjectLayout).join("");
+
+document.body.insertAdjacentHTML("beforeend", layout);
